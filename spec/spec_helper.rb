959339febcfe89b47ad4c801c94ed9b10a09b4a5
JaJune <jj.liu@iss.nthu.edu.tw>
2017-10-20 18:34:20 +0800

    Add Rakefile

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,10 +14,10 @@ require_relative '../lib/twitch_api.rb'
 
 CHANNELNAME = 'shroud'.freeze
 GAMENAME = 'CSGO'.freeze
-CONFIG = YAML.safe_load(File.read('../config/secret.yml'))
+CONFIG = YAML.safe_load(File.read('config/secret.yml'))
 TOKEN = CONFIG['token']
-#CLIP = JSON.parse(File.read('fixtures/clip.json'))
-GAME = JSON.parse(File.read('../spec/fixtures/sample/game.json'))
+# CLIP = JSON.parse(File.read('fixtures/clip.json'))
+GAME = JSON.parse(File.read('spec/fixtures/sample/game.json'))
 
-CASSETTES_FOLDER = '../spec/fixtures/cassettes'.freeze
+CASSETTES_FOLDER = 'spec/fixtures/cassettes'.freeze
 CASSETTE_FILE = 'twitch_api'.freeze
--- spec/twitch_spec.rb
+++ spec/twitch_spec.rb
@@ -1,18 +1,12 @@
 # frozen_string_literal: false
-=begin
-require 'minitest/autorun'
-require 'minitest/rg'
-require 'json'
-require 'yaml'
-require_relative '../lib/twitch_api.rb'
-=end
+
 require_relative 'spec_helper.rb'
 
 describe 'Tests Twitch library' do
   VCR.configure do |c|
     c.cassette_library_dir = CASSETTES_FOLDER
     c.hook_into :webmock
-
+    
     c.filter_sensitive_data('<Twitch_TOKEN>') { TOKEN }
     c.filter_sensitive_data('<Twitch_TOKEN_ESC>') { CGI.escape(TOKEN) }
   end
@@ -26,45 +20,18 @@ describe 'Tests Twitch library' do
   after do
     VCR.eject_cassette
   end
-=begin
-  CHANNELNAME = 'shroud'.freeze
-  GAMENAME = 'CSGO'.freeze
-  CONFIG = YAML.safe_load(File.read('../config/secret.yml'))
-  TOKEN = CONFIG['token']
-  #CLIP = JSON.parse(File.read('fixtures/clip.json'))
-  GAME = JSON.parse(File.read('../spec/fixtures/sample/game.json'))
-=end
-  # describe 'Clip information' do
-    # it 'HAPPY: should provide correct clip attributes' do
-    #   clip = Twitch::TwitchAPI.new(TOKEN).clip(CHANNELNAME)
-    #   _(clip.size).must_equal CLIP['clips'].count
-    # end
-
-    # it 'SAD: should raise exception on incorrect channel_name' do
-    #   proc do
-    #     Twitch::TwitchAPI.new(TOKEN).clip('shroood')
-    #   end.must_raise Twitch::TwitchAPI::Errors::NotFound
-    # end
-
-    # it 'SAD: should raise exception when unauthorized' do
-    #   proc do
-    #     Twitch::TwitchAPI.new('badtoken').clip(CHANNELNAME)
-    #   end.must_raise Twitch::TwitchAPI::Errors::BadRequest
-    # end
-  # end
 
   describe 'Game information' do
     it 'HAPPY: should provide correct game attributes' do
       game = Twitch::TwitchAPI.new(TOKEN).game(GAMENAME)
       _(game.num).must_equal GAME['_total']
+      _(game.name).must_equal GAME['game']
     end
-
     # it 'SAD: should raise exception on incorrect game_name' do
     #   proc do
-    #     Twitch::TwitchAPI.new(TOKEN).game(GAMENAME)
-    #   end.must_raise Twitch::TwitchAPI::Errors::NotFound
+    #    Twitch::TwitchAPI.new(TOKEN).game('IsntFun')
+    #   end.must_raise Twitch::Errors::NotFound
     # end
-
     it 'SAD: should raise exception when unauthorized' do
       proc do
         Twitch::TwitchAPI.new('badtoken').game(GAMENAME)
