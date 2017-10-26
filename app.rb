53ce97481a6977a5dd32140981ef9b193db2f4f9
silver926z <silver926z@gmail.com>
2017-10-27 03:41:21 +0800

    app.rb finished but too many lines

--- app.rb
+++ app.rb
@@ -18,44 +18,38 @@ module API
     route do |routing|
       app = Api
       config = Api.config
-
-      # GET / request
+      gh = Twitch::TwitchGateway.new(config.token)
+      
       routing.root do
         { 'message' => "Twitch API v0.1 up in #{app.environment}" }
       end
 
       routing.on 'api' do
         routing.on 'game', String do |game_name|
-          gh = Twitch::TwitchGateway.new(config.token)
           game_mapper = Twitch::GameMapper.new(gh)
           begin
             game = game_mapper.load(game_name)
           rescue StandardError
             routing.halt(404, error: 'game not found')
           end
-
           routing.is do
-            { info: game.to_h }
-          end
-
-          routing.get 'name' do
-            { name: game.name }
-          end
-
-          routing.get 'number' do
-            { number: game.number }
-          end
-
-          routing.get 'clips' do
-            { clips: game.clips.to_s }
-          end
-
-          routing.get 'streamers' do
-            { streamers: game.streamers }
+            game.to_h
           end
         end
 
-        
+        routing.on 'channel', String do |channel_name|
+          channel_mapper = Twitch::ChannelMapper.new(gh)
+
+          begin
+            channel = channel_mapper.load(channel_name)
+          rescue StandardError
+            routing.halt(404, error: 'channel not found')
+          end
+
+          routing.is do
+            channel.to_h
+          end
+        end
       end
     end
   end
